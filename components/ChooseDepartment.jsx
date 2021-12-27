6d21885c4c070a20e847ef729e407310201224b0
Majid Mido <majidmido03@gmail.com>
2021-12-27 19:13:31 +0100

    backend fix

--- components/ChooseDepartment.jsx
+++ components/ChooseDepartment.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
 
-const ChooseDepartment = ({year}) => {
-    return (
-        <div className="w-full h-full flex flex-col items-center justify-evenly shadow-xl">
-            <h1 className="text-4xl">{year.name}</h1>
-            {console.log(year.name)}
+import React from "react";
+
+const ChooseDepartment = ({ department,currentYear }) => {
+    
+    //{yearData.map((studyYear)=>{return(studyYear.node.departements.map((dep,index)=>{return(<ChooseDepartment key={index} currentYear={studyYear.node.currentYear} department={dep} />)}))})}
+  return (
+    <div className="w-full min-h-full flex flex-col items-center justify-evenly">
+        <div>
+
         </div>
-    )
-}
+      <div className="grid place-items-center">
+        <h1 className="text-4xl text-dark-200">{department.name}</h1>
+      </div>
+      <div className="grid grid-cols-3 grid-rows-3 place-items-center w-[80%] h-[80%]">
+        {department.drives.map((drv) => {
+          return (
+            <a href={drv.driveLink} target="_blank" className="shadow-lg text-dark-200 transition-all duration-300 hover:bg-light-200 hover:scale-105 hover:text-light w-full px-4 py-4 bg-light h-full flex flex-col justify-around items-center">
+              <h1 className="text-4xl">{drv.title}</h1>   
+            </a>
+          )
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default ChooseDepartment
+export default ChooseDepartment;
--- pages/year/index.js
+++ pages/year/index.js
@@ -1,18 +1,25 @@
 import React from "react";
-
+import { getDepartments } from "../../Services";
 import { ChooseDepartment } from "../../components";
 import Head from "next/head";
-
-const year = () => {
- 
+import Link from "next/link";
+const year = ({ yearData }) => {
   return (
-    <main className="w-full grid place-items-center min-h-screen snap-none  md:snap-y md:snap-proximity  bg-light-100">
+    <main className="w-full grid place-items-center min-h-screen snap-none  md:snap-y md:snap-proximity  bg-dark-200">
       <Head>
         <title>Pharma At Hand</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="w-full  min-h-screen grid place-items-center">
-        <h1 className="text-5xl text-dark-100 font-semibold">Coming Soon...</h1>
+      <div className="w-full gap-8 py-4 px-6  min-h-screen grid grid-cols-1 grid-rows-6 md:grid-cols-2 md:grid-rows-3 lg:grid-cols-3 lg:grid-rows-2  place-items-center">
+        {yearData.map((studyYear) => {
+          return (
+            <div className="grid h-full bg-gradient-to-r from-light-200 to-orange-300 text-4xl text-light transition-all duration-300 hover:scale-105  w-full place-items-center">
+              <Link href={`/year/${studyYear.node.currentYear}`} className="text-4xl text-light">
+                {studyYear.node.currentYear}
+              </Link>
+            </div>
+          );
+        })}
       </div>
     </main>
   );
@@ -20,3 +27,9 @@ const year = () => {
 
 export default year;
 
+export async function getStaticProps() {
+  const yearData = (await getDepartments()) || [];
+  return {
+    props: { yearData },
+  };
+}
