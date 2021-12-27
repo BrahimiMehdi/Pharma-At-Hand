a864e94af1854c43122cc06d227451234ac52a2d
Majid Mido <majidmido03@gmail.com>
2021-12-27 20:44:40 +0100

    test

--- components/ChooseDepartment.jsx
+++ components/ChooseDepartment.jsx
@@ -11,13 +11,7 @@ const ChooseDepartment = ({ department,currentYear }) => {
         <h1 className="text-4xl text-dark-200">{department.name}</h1>
       </div>
       <div className="grid grid-cols-3 grid-rows-3 place-items-center w-[80%] h-[80%]">
-        {department.drives.map((drv) => {
-          return (
-            <a href={drv.driveLink} target="_blank" className="shadow-lg text-dark-200 transition-all duration-300 hover:bg-light-200 hover:scale-105 hover:text-light w-full px-4 py-4 bg-light h-full flex flex-col justify-around items-center">
-              <h1 className="text-4xl">{drv.title}</h1>   
-            </a>
-          )
-        })}
+        
       </div>
     </div>
   );
