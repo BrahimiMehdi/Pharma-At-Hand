5ba17063bfa5109af0ff32f584a688c7902b2138
Majid Mido <majidmido03@gmail.com>
2021-12-27 06:57:54 +0100

    added a backend

---
+++ pages/year/[slug].js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const years = () => {
+  return (
+    <main className="w-full  min-h-screen snap-none  md:snap-y md:snap-proximity  bg-light-100">
+      <div className="min-h-screen w-full grid place-items-center">
+        <h1 className="text-5xl text-dark-100 font-semibold">Coming Soon...</h1>
+      </div>
+    </main>
+  );
+};
+
+export default years;
