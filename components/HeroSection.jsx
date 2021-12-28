22c5f14b573676c84f46e9bfcad2c32fae1d1ecc
Majid Mido <majidmido03@gmail.com>
2021-12-28 10:31:26 +0100

    work?

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -4,7 +4,7 @@ import { Link as LinkS } from "react-scroll";
 import { gsap, Power3 } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 const HeroSection = () => {
-    gsap.registerPlugin(ScrollTrigger)
+  gsap.registerPlugin(ScrollTrigger);
   const heroImage = useRef();
   const heroInfo = useRef();
   const outerDiv = useRef();
@@ -20,11 +20,19 @@ const HeroSection = () => {
       ease: Power3.easeInOut,
       duration: 1,
     });
-    gsap.to(outerDiv.current,{opacity:0,scrollTrigger:{trigger:"#aboutSection",start:"top bottom",toggleActions: "restart pause resume none",scrub:1}})
+    gsap.to(outerDiv.current, {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: "#aboutSection",
+        start: "top bottom",
+        toggleActions: "restart pause resume none",
+        scrub: 1,
+      },
+    });
   }, []);
   return (
     <section
-        ref={outerDiv}
+      ref={outerDiv}
       name="/"
       className="grid  overflow-x-hidden md:snap-center place-items-center w-full min-h-screen pt-14 sm:pt-0"
     >
@@ -42,17 +50,19 @@ const HeroSection = () => {
             installed.
           </p>
           <div id="heroButtons" className="w-full flex mt-4 items-center ">
-            <span className="bg-white  border-[1px] transition-all duration-300 active:scale-95 hover:scale-105 active:bg-light-200 hover:bg-light-100 mr-4 border-dark-200 py-2 text-center text-dark-100 w-[150px]">
-              <Link className="w-full mx-4 h-full" href="/year">GET STARTED</Link>
-            </span>
-            <span className="text-white  border-[1px] ml-4 transition-all duration-300 active:scale-95 hover:scale-105 active:bg-light-200 hover:bg-dark-200 border-dark-200 py-2 text-center bg-dark-100 w-[150px] ">
-              <LinkS
-                className="w-full cursor-pointer mx-4 h-full"
-                to="/about"
-              >
+            <Link className="cursor-pointer" href={`/year`}>
+              <span className="bg-white cursor-pointer  border-[1px] transition-all duration-300 active:scale-95 hover:scale-105 active:bg-light-200 hover:bg-light-100 mr-4 border-dark-200 py-2 text-center text-dark-100 w-[150px]">
+                GET STARTED
+              </span>
+            </Link>
+            <LinkS
+              className="grid place-items-center text-center"
+              to={`/about`}
+            >
+              <span className="text-white cursor-pointer  border-[1px] ml-4 transition-all duration-300 active:scale-95 hover:scale-105 active:bg-light-200 hover:bg-dark-200 border-dark-200 py-2 text-center bg-dark-100 w-[150px] ">
                 SEE MORE
-              </LinkS>
-            </span>
+              </span>
+            </LinkS>
           </div>
         </div>
         <div
